e037e2e134c041749c83cfda96272c2a9c469093
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-09-16 10:58:31 -0700

    ci: init nix

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.follows = "nixpkgs-stable";
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-unstable";
+        nixpkgs-stable.follows = "nixpkgs-stable";
+      };
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.git-hooks-nix.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { config, ... }:
+        {
+
+          treefmt = {
+            programs = {
+              nixfmt.enable = true;
+              shfmt.enable = true;
+            };
+            projectRootFile = "flake.nix";
+          };
+
+          pre-commit.settings = {
+            hooks = {
+              treefmt = {
+                enable = true;
+                package = config.treefmt.build.wrapper;
+              };
+              shellcheck = {
+                enable = true;
+              };
+            };
+          };
+
+          devshells.default = {
+            commands = [
+              {
+                help = "run all checks";
+                name = "ci-check";
+                command =
+                  # bash
+                  ''
+                    pushd $PRJ_ROOT
+                    nix flake check
+                    popd
+                  '';
+              }
+            ];
+          };
+
+          devShells.pre-commit = config.pre-commit.devShell;
+        };
+    };
+}
